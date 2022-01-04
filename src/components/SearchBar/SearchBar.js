26a21343bd29e294eadc0d0c04198d765332eb60
pseno <pasalicseno@hotmail.com>
2022-01-04 18:01:52 +0100

    modified file

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -80,9 +80,7 @@ class SearchBar extends React.Component {
           <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
-          <a href="www.#.com" onClick={this.handleSearch}>
-            Let's Go
-          </a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
