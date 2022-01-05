c70d403a23c936043b4fba595f2f20a7481d483e
pseno <pasalicseno@hotmail.com>
2022-01-05 13:57:20 +0100

    modified files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,28 @@
 import './App.css';
 import React from 'react';
+import Yelp from '../../util/Yelp';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-const businesses = [business, business, business, business, business, business];
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   searchYelp(term, location, sortBy) {
-    console.log(`You are searching for ${term}, ${location} and ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then((businesses) => {
+      this.state({ businesses: businesses });
+    });
   }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
